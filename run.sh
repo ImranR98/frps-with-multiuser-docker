583d3832bb18908ffdf5b378e346c1d202544b05
Imran Remtulla <contact@imranr.dev>
2025-09-14 12:53:56 -0400

    Switch to TOML config

--- run.sh
+++ run.sh
@@ -4,11 +4,11 @@ set -e
 
 mkdir -p /workdir/service-data
 
-if [ ! -f /workdir/service-data/frps.ini ]; then
-    cp /workdir/default.frps.ini /workdir/service-data/frps.ini
+if [ ! -f /workdir/service-data/frps.toml ]; then
+    cp /workdir/default.frps.toml /workdir/service-data/frps.toml
 fi
 
-/usr/local/bin/frps -c /workdir/service-data/frps.ini &
+/usr/local/bin/frps -c /workdir/service-data/frps.toml &
 /usr/local/bin/frps-multiuser -l 127.0.0.1:7200 -f /workdir/service-data/frps-tokens.txt &
 
 wait -n
